b7ea89fe7b6b1e2226081015a17817ff2839201f
Carlitosh636 <61882229+Carlitosh636@users.noreply.github.com>
2020-11-02 12:20:27 +0100

    Funcion Salir Menu Principal

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,13 +3,29 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    View menuPrincipal=findViewById(R.id.activity_main);
+    Button botonJugar,botonOpciones,botonSalir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        botonSalir=(Button) findViewById(R.id.BotonSalir);
+    }
+    private void FuncionTestSalir(){ //BORRAR EN EL FUTURO
+        botonSalir.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
     }
 }
 

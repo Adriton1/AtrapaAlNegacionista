2b7e64b9380caf4ac18c64c7af412980ef60fc92
Adrian <adri-lc2011@hotmail.com>
2020-11-03 14:49:27 +0100

    comentario prueba rama menupp

--- app/src/main/java/com/example/myapplication/OpcionesAct.java
+++ app/src/main/java/com/example/myapplication/OpcionesAct.java
@@ -10,5 +10,6 @@ public class OpcionesAct extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menuopciones);
+        //prueba rama menupp
     }
 }

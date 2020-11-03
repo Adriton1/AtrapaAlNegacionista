befb7245a7b8650580e43e582a28d96c0b813350
JSPINDEV <javier.espin@hotmail.es>
2020-11-03 14:43:30 +0100

    Espacio

--- app/src/main/java/com/example/myapplication/OpcionesAct.java
+++ app/src/main/java/com/example/myapplication/OpcionesAct.java
@@ -10,5 +10,5 @@ public class OpcionesAct extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menuopciones);
-    }
+    }  
 }

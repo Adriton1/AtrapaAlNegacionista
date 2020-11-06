b0b915848f52d21734c74c9923b215d2c5a63367
Carlitosh636 <61882229+Carlitosh636@users.noreply.github.com>
2020-11-06 16:30:36 +0100

    RefactorizaciónFuncionesJuego1
    Limpieza y refactorización de aleatorio y cuentaatrás

--- app/src/main/java/com/example/myapplication/MainJuego.java
+++ app/src/main/java/com/example/myapplication/MainJuego.java
@@ -31,8 +31,8 @@ public class MainJuego extends AppCompatActivity {
             cuentaatras(tiempRest);
         }
         else {
-            aleatorio();
-            cuentaatras();
+            aleatorio(0);
+            cuentaatras((long) 20);
         }
     }
 
@@ -43,49 +43,6 @@ public class MainJuego extends AppCompatActivity {
         outState.putInt("caso",casoconcreto);
     }
 
-
-    public void aleatorio(){
-        //Código temporal. Inicializamos todas la imágenes
-        ImageView imagen11 =  findViewById(R.id.imag11);    //Infectado posicion 1
-        ImageView imagen12 =  findViewById(R.id.imag12);    //Mascarilla bien puesta posicion 1
-        ImageView imagen21 =  findViewById(R.id.imag21);    //Infectado posicion 2
-        ImageView imagen22 =  findViewById(R.id.imag22);    //Mascarilla bien puesta posicion 2
-        ImageView imagen31 =  findViewById(R.id.imag31);    //Infectado posicion 3
-        ImageView imagen32 =  findViewById(R.id.imag32);    //Mascarilla bien puesta posicion 3
-        ImageView imagen41 =  findViewById(R.id.imag41);    //Infectado posicion 4
-        ImageView imagen42 =  findViewById(R.id.imag42);    //Mascarilla bien puesta posicion 4
-
-        Random random= new Random();
-        int num= random.nextInt(4);
-        casoconcreto=num;
-        switch (num){
-            case 0:
-                imagen11.setVisibility(View.VISIBLE);
-                imagen22.setVisibility(View.VISIBLE);
-                imagen32.setVisibility(View.VISIBLE);
-                imagen42.setVisibility(View.VISIBLE);
-                break;
-            case 1:
-                imagen12.setVisibility(View.VISIBLE);
-                imagen21.setVisibility(View.VISIBLE);
-                imagen32.setVisibility(View.VISIBLE);
-                imagen42.setVisibility(View.VISIBLE);
-                break;
-            case 2:
-                imagen12.setVisibility(View.VISIBLE);
-                imagen22.setVisibility(View.VISIBLE);
-                imagen31.setVisibility(View.VISIBLE);
-                imagen42.setVisibility(View.VISIBLE);
-                break;
-            case 3:
-                imagen12.setVisibility(View.VISIBLE);
-                imagen22.setVisibility(View.VISIBLE);
-                imagen32.setVisibility(View.VISIBLE);
-                imagen41.setVisibility(View.VISIBLE);
-                break;
-        }
-
-    }
     public void onClick(View view){
 
         switch (view.getId()){   //cambia al pesonaje en la casilla marcada
@@ -121,11 +78,11 @@ public class MainJuego extends AppCompatActivity {
         }
 
     }
-    //public void haGanado (boolean hasGanado) {
-        //if (hasGanado == false) {
-           // startActivity(new Intent(MainJuego.this, PantallaDerrota.class));
-       // }
-   // }
+    /*public void haGanado (boolean hasGanado) {
+        if (hasGanado == false) {
+            startActivity(new Intent(MainJuego.this, PantallaDerrota.class));
+        }
+    }*/
 
 
     public void FuncionIrOpcionesJuego(View view) {
@@ -138,25 +95,6 @@ public class MainJuego extends AppCompatActivity {
         });
     }
 
-    public void cuentaatras() {
-        //Se trata de un contador simple si se termina el tiempo se acaba el juego
-        contador = findViewById(R.id.cuentaAtras);
-        final long duracionTotal= 20 * 1000; //milisegundos
-        final long tiempoEntreTicks= 1000; //un segundo
-        new CountDownTimer(duracionTotal, tiempoEntreTicks) {
-            public void onTick(long milisegHastaFin) {
-                long segRest = (milisegHastaFin) /1000;
-                tiempRest=segRest;
-                contador.setText("Quedan: "+String.valueOf(segRest)+" segundos!!!");
-            }
-
-            public void onFinish() {
-                contador.setText("Has perdido!!");
-                //Te mostraría una pantalla que pone has perdido, con la opción de volver a jugar o salir de la app
-            }
-        }.start();
-    }
-
     public void aleatorio(Integer caso){
         //Código temporal. Inicializamos todas la imágenes
         ImageView imagen11 =  findViewById(R.id.imag11);    //Infectado posicion 1
@@ -167,8 +105,15 @@ public class MainJuego extends AppCompatActivity {
         ImageView imagen32 =  findViewById(R.id.imag32);    //Mascarilla bien puesta posicion 3
         ImageView imagen41 =  findViewById(R.id.imag41);    //Infectado posicion 4
         ImageView imagen42 =  findViewById(R.id.imag42);    //Mascarilla bien puesta posicion 4
-
-        casoconcreto=caso;
+        int num;
+        if(caso==0) {
+            Random random = new Random();
+            num = random.nextInt(4);
+        }
+        else {
+            num=caso;
+        }
+        casoconcreto=num;
 
         switch (caso){
             case 0:

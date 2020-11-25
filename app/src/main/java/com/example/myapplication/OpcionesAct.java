b27ef1d3cb6ff0fbc612a48bf47420e1db53e799
JSPINDEV <javier.espin@hotmail.es>
2020-11-25 19:47:22 +0100

    musica opciones en rama music

--- app/src/main/java/com/example/myapplication/OpcionesAct.java
+++ app/src/main/java/com/example/myapplication/OpcionesAct.java
@@ -3,14 +3,22 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class OpcionesAct extends AppCompatActivity {
     ImageButton cruz;
     Button guardar, volverAlMenu;
+
+
+    Button play_pause;
+    MediaPlayer mp;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +27,33 @@ public class OpcionesAct extends AppCompatActivity {
         guardar=(Button) findViewById(R.id.guardar);
         volverAlMenu=(Button) findViewById(R.id.volverAlMenu);
         //prueba rama menupp
+
+        play_pause=(Button)findViewById(R.id.play_pause); //crear el boton en opciones y ponerle ese id
+
+        mp = MediaPlayer.create(this,R.raw.musicajuego);  //esta va al  iniciar la aplicacion
+
+
+
     }
+
+
+      public void playpause(View view){
+
+          play_pause.setOnClickListener(new View.OnClickListener() { //esto va fuera
+                @Override
+                public void onClick(View v) {
+                    if(mp.isPlaying()){
+                        mp.pause();
+                        Toast.makeText(OpcionesAct.this, "Pausa" , Toast.LENGTH_SHORT).show();   //mirar xq esto es un mensaje  en popup y el main creo que habria que quitarlo
+                    }else{
+                        mp.start();
+                        Toast.makeText(OpcionesAct.this, "Play" , Toast.LENGTH_SHORT).show();
+                   }
+                }
+           });
+     }
+
+
     public void volverAlmenu(View view){
         volverAlMenu.setOnClickListener(new View.OnClickListener() {
             @Override

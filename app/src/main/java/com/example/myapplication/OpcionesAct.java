26f98edc3455a9fea1da9d378a09919c0d21ae23
Ismaelgzse <61881942+Ismaelgzse@users.noreply.github.com>
2020-12-09 18:09:51 +0100

    Update OpcionesAct.java

--- app/src/main/java/com/example/myapplication/OpcionesAct.java
+++ app/src/main/java/com/example/myapplication/OpcionesAct.java
@@ -11,6 +11,7 @@ import android.widget.ImageButton;
 public class OpcionesAct extends AppCompatActivity {
     ImageButton cruz;
     Button guardar, volverAlMenu;
+    public static boolean encendida;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,8 +19,52 @@ public class OpcionesAct extends AppCompatActivity {
         cruz= (ImageButton) findViewById(R.id.cruz);
         guardar=(Button) findViewById(R.id.guardar);
         volverAlMenu=(Button) findViewById(R.id.volverAlMenu);
-        //prueba rama menupp
+        musica=(Button) findViewById(R.id.botonMusica);
+        if (savedInstanceState ==null){
+                encendida=true;
+        }
+        else{
+            encendida=savedInstanceState.getBoolean("estado");
+        }
+        
     }
+        @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean("estado",encendida);
+    }
+
+    public void FuncionesMusica(View view) {
+        musica = (Button) findViewById(R.id.botonMusica);
+
+        musica.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (encendida) {
+                    apagaMusica();
+                } else {
+                    enciendeMusica();
+                }
+            }
+        });
+    }
+    public void enciendeMusica (){
+
+        Intent  mpI = new Intent(this, ServicioMusica.class);
+
+        this.startService(mpI);
+
+        encendida = !encendida;
+    }
+
+    public void apagaMusica(){
+        Intent  mpI = new Intent(this, ServicioMusica.class);
+
+        this.stopService(mpI);
+
+        encendida = !encendida;
+    }
+
     public void volverAlmenu(View view){
         volverAlMenu.setOnClickListener(new View.OnClickListener() {
             @Override

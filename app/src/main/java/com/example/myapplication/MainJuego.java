9fd3c7d67de39527ee4b60536e4c354f0852df5f
Carlitosh636 <61882229+Carlitosh636@users.noreply.github.com>
2020-12-10 18:21:18 +0100

    CodigoMainJuego

--- app/src/main/java/com/example/myapplication/MainJuego.java
+++ app/src/main/java/com/example/myapplication/MainJuego.java
@@ -34,23 +34,29 @@ public class MainJuego extends AppCompatActivity {
         ArrayList<ImageView> personas=new ArrayList<>();
         GridLayout miGrid= (GridLayout) findViewById(R.id.Grid);
         int count=miGrid.getChildCount();
-        int id,number;
+        int id,number,badSelect;
         Random ran=new Random();
+        badSelect=ran.nextInt(7-1)+1;
         for (int i=0;i<count;i++){
             ImageView hijo=(ImageView) miGrid.getChildAt(i);
             number=ran.nextInt(5-1)+1;
-            hijo.setImageResource(getImage("bien"+number));
+            if(i==badSelect){
+                hijo.setImageResource(getImage("mal"+number));
+                hijo.setTag("PersonaContagiada");
+            }
+            else {
+                hijo.setImageResource(getImage("bien" + number));
+                hijo.setTag("Persona");
+            }
             personas.add(hijo);
         }
 
         if (savedInstanceState !=null){
             tiempRest=savedInstanceState.getLong("segundos");
             casoconcreto= savedInstanceState.getInt("caso");
-            aleatorio(casoconcreto);
             cuentaatras(tiempRest);
         }
         else {
-            aleatorio(9);
             cuentaatras((long) 20);
         }
     }
@@ -67,45 +73,25 @@ public class MainJuego extends AppCompatActivity {
     }
 
     public void onClick(View view){
+        int miID=view.getId();
+        ImageView laImagen=(ImageView) findViewById(miID);
+        if(laImagen.getTag()=="PersonaContagiada"){
+            contador.setText("Has encontrado al negacionista!!");
+            hasGanado=true;
+        }
+        else{
 
-        /*switch (view.getId()){   //cambia al pesonaje en la casilla marcada
-            case R.id.imag11:
-                ImageView imagen11 = (ImageView) findViewById(R.id.imag11);
-                imagen11.setImageResource(R.drawable.e);    //cambia la imagen con la de la mascarilla
-                contador.setText("Has encontrado al negacionista!!");   //Escribe en contador
-                //startActivity(new Intent(MainJuego.this, PantallaVictoria.class));
-                hasGanado=true;
-                break;
-            case R.id.imag21:
-                ImageView imagen21 = (ImageView) findViewById(R.id.imag21);
-                imagen21.setImageResource(R.drawable.e);
-                contador.setText("Has encontrado al negacionista!!");
-                //startActivity(new Intent(MainJuego.this, PantallaVictoria.class));
-                hasGanado=true;
-                break;
-            case R.id.imag31:
-                ImageView imagen31 = (ImageView) findViewById(R.id.imag31);
-                imagen31.setImageResource(R.drawable.e);
-                contador.setText("Has encontrado al negacionista!!");
-                //startActivity(new Intent(MainJuego.this, PantallaVictoria.class));
-                hasGanado=true;
-                break;
-            case R.id.imag41:
-                ImageView imagen41 = (ImageView) findViewById(R.id.imag41);
-                imagen41.setImageResource(R.drawable.e);
-                contador.setText("Has encontrado al negacionista!!");
-                //startActivity(new Intent(MainJuego.this, PantallaVictoria.class));
-                hasGanado=true;
-                break;
-
-        }*/
+        }
 
     }
-    /*public void haGanado (boolean hasGanado) {
+    public void haGanado (boolean hasGanado) {
         if (hasGanado == false) {
             startActivity(new Intent(MainJuego.this, PantallaDerrota.class));
         }
-    }*/
+        else{
+            startActivity(new Intent(MainJuego.this, PantallaVictoria.class));
+        }
+    }
 
 
     public void FuncionIrOpcionesJuego(View view) {
@@ -118,54 +104,6 @@ public class MainJuego extends AppCompatActivity {
         });
     }
 
-    public void aleatorio(Integer caso){
-       /* //Código temporal. Inicializamos todas la imágenes
-        ImageView imagen11 =  findViewById(R.id.imag11);    //Infectado posicion 1
-        ImageView imagen12 =  findViewById(R.id.imag12);    //Mascarilla bien puesta posicion 1
-        ImageView imagen21 =  findViewById(R.id.imag21);    //Infectado posicion 2
-        ImageView imagen22 =  findViewById(R.id.imag22);    //Mascarilla bien puesta posicion 2
-        ImageView imagen31 =  findViewById(R.id.imag31);    //Infectado posicion 3
-        ImageView imagen32 =  findViewById(R.id.imag32);    //Mascarilla bien puesta posicion 3
-        ImageView imagen41 =  findViewById(R.id.imag41);    //Infectado posicion 4
-        ImageView imagen42 =  findViewById(R.id.imag42);    //Mascarilla bien puesta posicion 4
-        int num;
-        if(caso==9) {
-            Random random = new Random();
-            num = random.nextInt(4);
-        }
-        else {
-            num=caso;
-        }
-        casoconcreto=num;
-
-        switch (num){
-            case 0:
-                imagen11.setVisibility(View.VISIBLE);
-                imagen22.setVisibility(View.VISIBLE);
-                imagen32.setVisibility(View.VISIBLE);
-                imagen42.setVisibility(View.VISIBLE);
-                break;
-            case 1:
-                imagen12.setVisibility(View.VISIBLE);
-                imagen21.setVisibility(View.VISIBLE);
-                imagen32.setVisibility(View.VISIBLE);
-                imagen42.setVisibility(View.VISIBLE);
-                break;
-            case 2:
-                imagen12.setVisibility(View.VISIBLE);
-                imagen22.setVisibility(View.VISIBLE);
-                imagen31.setVisibility(View.VISIBLE);
-                imagen42.setVisibility(View.VISIBLE);
-                break;
-            case 3:
-                imagen12.setVisibility(View.VISIBLE);
-                imagen22.setVisibility(View.VISIBLE);
-                imagen32.setVisibility(View.VISIBLE);
-                imagen41.setVisibility(View.VISIBLE);
-                break;
-        }*/
-
-    }
     public void cuentaatras(Long tiemp) {
         //Se trata de un contador simple si se termina el tiempo se acaba el juego
         contador = findViewById(R.id.cuentaAtras);
@@ -175,11 +113,12 @@ public class MainJuego extends AppCompatActivity {
             public void onTick(long milisegHastaFin) {
                 long segRest = (milisegHastaFin) /1000;
                 tiempRest=segRest;
-                contador.setText("Quedan: "+String.valueOf(segRest)+" segundos!!!");
+                if(!hasGanado)
+                    contador.setText("Quedan: "+String.valueOf(segRest)+" segundos!!!");
             }
 
             public void onFinish() {
-                contador.setText("Has perdido!!");
+                haGanado(hasGanado);
                 //Te mostraría una pantalla que pone has perdido, con la opción de volver a jugar o salir de la app
             }
         }.start();
